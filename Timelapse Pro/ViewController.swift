2fc6bccbf90aca5f05ac7e0620f7492bddba7048
skuehnl <simonkuhnl@Simons-MacBook-Pro.local>
2017-09-19 18:14:19 +0200

    Initial Commit

---
+++ Timelapse Pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timelapse Pro
+//
+//  Created by Simon Kühnl on 19.09.17.
+//  Copyright © 2017 flying-raspberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
